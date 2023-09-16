b5dc31345951938055f3379505b903d74280677a
Oskar Svahn <oskar.svahn1@gmail.com>
2023-09-16 18:07:27 +0200

    convert to class

--- labeling/labeling.py
+++ labeling/labeling.py
@@ -3,104 +3,106 @@ import pandas as pd
 import numpy as np
 import os
 
-# ffmpeg -i videos/mouse.mkv -c:v copy -c:a aac -strict experimental output.mp
-# ffmpeg -i output.mp4 -vf mpdecimate -vsync vfr out_kompressed.mp4
+class VideoAnnotation:
+    def __init__(self, video_path, csv_name, num_points=8):
+        self.video_path = os.path.join(os.path.dirname(__file__), "..", video_path).replace('\\', '/')  
+        self.csv_name = os.path.join(os.path.dirname(__file__), csv_name).replace('\\', '/')  
+        self.num_points = num_points
+        self.clicked_points = []
+        self.current_frame = 0
 
-video_path = "vidoes/out_kompressed.mp4"
+        self.cap = cv2.VideoCapture(self.video_path)
 
-csv_name = "out"
+        if not self.cap.isOpened():
+            print("Error: Could not open video file.")
+            exit()
 
-NUM_POINTS = 8
+        try:
+            self.df_points = pd.read_csv(self.csv_name)
+            self.current_frame = self.df_points["Frame_ID"].max()
+            if np.isnan(self.current_frame):
+                self.current_frame = 0
+        except:
+            self.df_points = pd.DataFrame(columns=["Frame_ID"] + [f"Point_{i+1}_{axis}" for i in range(self.num_points) for axis in ["X", "Y"]])
 
-clicked_points = []
-current_frame = 0
+        cv2.namedWindow("Video Frame")
+        cv2.setMouseCallback("Video Frame", self.mouse_callback)
 
+    def hsv_to_bgr(self, hsv_color):
+        return tuple(int(x) for x in cv2.cvtColor(np.uint8([[hsv_color]]), cv2.COLOR_HSV2BGR)[0][0])
 
-def hsv_to_bgr(hsv_color):
-    return tuple(int(x) for x in cv2.cvtColor(np.uint8([[hsv_color]]), cv2.COLOR_HSV2BGR)[0][0])
+    def show_cur_points(self):
+        for i, (x, y) in enumerate(self.clicked_points):
+            cv2.circle(self.frame, (x, y), 5, self.hsv_to_bgr((i*20, 255, 255)), -1)  
+        cv2.imshow("Video Frame", self.frame)
 
-def show_cur_points():
-    for i, (x, y) in enumerate(clicked_points):
-        print(x, y)
-        cv2.circle(frame, (x, y), 5, hsv_to_bgr((i*20, 255, 255)), -1)  
-    cv2.imshow("Video Frame", frame)
+    def get_frame(self):
+        self.cap.set(cv2.CAP_PROP_POS_FRAMES, self.current_frame)
+        ret, self.frame = self.cap.read()
+        if not ret:
+            return None
+        return self.frame
 
+    def mouse_callback(self, event, x, y, flags, param):
+        if event == cv2.EVENT_LBUTTONDOWN:
+            self.clicked_points.append((x, y))
+            self.show_cur_points()
 
-def mouse_callback(event, x, y, flags, param):
-    if event == cv2.EVENT_LBUTTONDOWN:
-        clicked_points.append((x, y))
-        show_cur_points()
+    def annotate_video(self):
+        while True:
+            self.frame = self.get_frame()
+            if self.frame is None:
+                break
 
+            # Display previously clicked points
+            if (self.df_points["Frame_ID"] == self.current_frame).any():
+                points = self.df_points.loc[self.df_points["Frame_ID"] == self.current_frame]
+                for i in range(self.num_points):
+                    cv2.circle(self.frame, (points[f"Point_{i+1}_X"].item(), points[f"Point_{i+1}_Y"].item()), 5, self.hsv_to_bgr((i*20, 255, 255)), -1)
 
+            self.show_cur_points()
 
-cap = cv2.VideoCapture(video_path)
+            key = cv2.waitKey(0) & 0xFF
+            if key == ord('q'):
+                break
+            if key == ord('s'):
+                # Save the clicked points to the CSV file
+                if len(self.clicked_points) == self.num_points:
+                    point_dict = {"Frame_ID": self.current_frame}
+                    for i, (x, y) in enumerate(self.clicked_points):
+                        point_dict[f"Point_{i+1}_X"] = x
+                        point_dict[f"Point_{i+1}_Y"] = y
 
-if not cap.isOpened():
-    print("Error: Could not open video file.")
-    exit()
+                    if (self.df_points["Frame_ID"] == self.current_frame).any():
+                        self.df_points.loc[self.df_points["Frame_ID"] == self.current_frame] = pd.DataFrame([point_dict])
+                    else:
+                        self.df_points = pd.concat([self.df_points, pd.DataFrame([point_dict])], ignore_index=True)
 
-try:
-    df_points = pd.read_csv(f"csv_files/{csv_name}.csv")
-    current_frame = df_points["Frame_ID"].max()
-    if current_frame != current_frame:  # is_nan
-        current_frame = 0
-except:    
-    df_points = pd.DataFrame(columns = ["Frame_ID"] + [f"Point_{i+1}_{axis}" for i in range(NUM_POINTS) for axis in ["X", "Y"]])
+                    self.df_points = self.df_points.sort_values(by=["Frame_ID"])
+                    print(f"{self.num_points} points saved to DataFrame.")
+                    self.df_points.to_csv(self.csv_name, index=False)
+                    self.clicked_points = []
+                    self.current_frame += 1
 
+                else:
+                    print(f"Please click on {self.num_points} points.")
 
-cv2.namedWindow("Video Frame")
-cv2.setMouseCallback("Video Frame", mouse_callback)
-while True:
-    cap.set(cv2.CAP_PROP_POS_FRAMES, current_frame)
-    ret, frame = cap.read()
-    if not ret:
-        break
+            elif key == ord('d'): 
+                self.current_frame += 1
+                self.clicked_points = []
+            elif key == ord('a'):
+                self.current_frame -= 1
+                self.clicked_points = []
 
-    # Display previously clicked points
-    if (df_points["Frame_ID"]==current_frame).any():
-        points = df_points.loc[df_points["Frame_ID"]==current_frame]
-        for i in range(NUM_POINTS):
-            cv2.circle(frame, (points[f"Point_{i+1}_X"].item(), points[f"Point_{i+1}_Y"].item()), 5, hsv_to_bgr((i*20, 255, 255)), -1)
-    
-    show_cur_points()
+            if key == ord('b'):
+                if self.clicked_points:
+                    self.clicked_points.pop()
+        self.cap.release()
 
-    key = cv2.waitKey(0) & 0xFF
-    if key == ord('q'):
-        break
-    if key == ord('s'):
-        # Save the clicked points to the CSV file
-        if len(clicked_points) == NUM_POINTS:
-            point_dict = {"Frame_ID": current_frame}
-            for i, (x, y) in enumerate(clicked_points):
-                point_dict[f"Point_{i+1}_X"] = x
-                point_dict[f"Point_{i+1}_Y"] = y
+if __name__ == "__main__":
+    video_path = "videos/out_kompressed.mp4"
+    csv_name = "csv_files/out.csv"
+    num_points = 8
 
-            if (df_points["Frame_ID"] == current_frame).any():
-                df_points.loc[df_points["Frame_ID"] == current_frame] =pd.DataFrame([point_dict])
-            else:
-                df_points = pd.concat([df_points, pd.DataFrame([point_dict])], ignore_index=True)
-
-            df_points = df_points.sort_values(by=["Frame_ID"])
-            print(f"{NUM_POINTS} points saved to DataFrame.")
-            df_points.to_csv(f"csv_files/{csv_name}.csv", index=False)
-            clicked_points = []
-            current_frame += 1
-
-        else:
-            print(f"Please click on {NUM_POINTS} points.")
-
-    elif key == ord('d'): 
-        current_frame += 1
-        clicked_points = []
-
-        print(current_frame)
-    elif key == ord('a'):
-        current_frame -= 1
-        clicked_points = []
-
-        print(current_frame)
-
-    if key == ord('b'):
-        if clicked_points:
-            clicked_points.pop()
-cap.release()
+    annotator = VideoAnnotation(video_path, csv_name, num_points)
+    annotator.annotate_video()
