c84fea76a11d9cc1d6081a48581e988483a36998
johmag2 <86012976+johmag2@users.noreply.github.com>
2023-11-19 12:14:30 +0100

    Worked on generall layout

--- GUI/GUI_template.py
+++ GUI/GUI_template.py
@@ -29,15 +29,18 @@ class MainWindow(QMainWindow):
         #centralWidget.setStyleSheet("background-color: midnightblue;")
         centralWidget.setLayout(self.generalLayout)
         self.setCentralWidget(centralWidget)
-        self.imageDisplay()
-        self.createButtons()
-        self.createMenuBar()
-        self.createListView()
         
-        self.setMinimumSize(1000, 600)
+        self.generalLayout.addWidget(self.createListView(),0,0,2,1)
+        self.generalLayout.addLayout(self.imageDisplay(),0,1,1,1)  
+        self.generalLayout.addLayout(self.imageInfo(),1,1,1,1)  
+        
+        self.createMenuBar()
+        
+        #self.setMinimumSize(1000, 600)
+        self.setFixedSize(1000, 600)
         self.show()
             
-    def drawEllipse(self,x,y,radii=5):
+    def drawEllipse(self,x,y,radii=1):
         painter = QtGui.QPainter(self.image.pixmap())
         pen = QtGui.QPen()
         pen.setColor(QtGui.QColor('yellow'))
@@ -54,13 +57,13 @@ class MainWindow(QMainWindow):
         self.list_widget.setFixedWidth(450)
         self.list_widget.setSpacing(0)
         self.list_widget.setUniformItemSizes(True)
-        self.generalLayout.addWidget(self.list_widget, 0,0)
+        
+        return self.list_widget
     
     def selectListItem(self,item):
         name = item.text()
         self.displayImage(self.folder_path+"/"+name)
         self.current_index = self.pictures.index(name)
-        
         
     def addToList(self,item):
         
@@ -73,84 +76,123 @@ class MainWindow(QMainWindow):
         
     
     def createMenuBar(self):
-        menuBar = self.menuBar()
+        menu_bar = self.menuBar()
         
-        fileMenu = menuBar.addMenu("&File")
-        editMenu = menuBar.addMenu("&Edit")
-        helpMenu = menuBar.addMenu("&Help")
+        file_menu = menu_bar.addMenu("&File")
+        edit_menu = menu_bar.addMenu("&Edit")
+        help_menu = menu_bar.addMenu("&Help")
+        tempMenu = menu_bar.addMenu("&Temp")
         
-        self.new = fileMenu.addAction("New Project")
-        self.save = fileMenu.addAction("Save Project")
-        self.blank = fileMenu.addAction("")
+        self.new = file_menu.addAction("New Project")
+        pixmapi = QStyle.StandardPixmap.SP_FileDialogNewFolder
+        icon = self.style().standardIcon(pixmapi)
+        self.new.setIcon(icon)
+        
+        self.save = file_menu.addAction("Save Project")
+        pixmapi = QStyle.StandardPixmap.SP_DialogSaveButton
+        icon = self.style().standardIcon(pixmapi)
+        self.save.setIcon(icon)
+        
+        self.blank = file_menu.addAction("")
         
         
         #self.load.triggered.connect(self.loadAction)
         
-        #fileMenu.addAction(self.openAction)
-        #fileMenu.addAction(self.saveAction)
-        #fileMenu.addAction(self.exitAction)
-    
-    def loadAction(self):
-        self.selectFolder()
-    
-    def createButtons(self):
-        self.buttonMap = {}
-        buttonsLayout = QtWidgets.QGridLayout()
+        load_image = tempMenu.addAction("Select Image")
+        load_image.triggered.connect(self.selectImage)
         
-        imageButton = QPushButton("Select Image")
-        imageButton.setFixedSize(100,30)
-        imageButton.clicked.connect(self.selectImage)
-        buttonsLayout.addWidget(imageButton,1,0)
+        load_folder = tempMenu.addAction("Select Folder")
+        load_folder.triggered.connect(self.selectFolder)
         
-        folderButton = QPushButton("Select Folder")
-        folderButton.setFixedSize(100,30)
-        folderButton.clicked.connect(self.selectFolder)
-        buttonsLayout.addWidget(folderButton,1,1)
+        load_csv = tempMenu.addAction("Select CSV")
+        load_csv.triggered.connect(self.loadPoints)
         
-        csvButton = QPushButton("Select CSV")
-        csvButton.setFixedSize(100,30)
-        csvButton.clicked.connect(self.loadPoints)
-        buttonsLayout.addWidget(csvButton,2,0)
-        
-        pointButton = QPushButton("Show Points")
-        pointButton.setFixedSize(100,30)
-        pointButton.clicked.connect(self.showPoints)
-        buttonsLayout.addWidget(pointButton,2,1)
-        
-        self.generalLayout.addLayout(buttonsLayout,1,0,1,2)
-    
+        show_points = tempMenu.addAction("Show Points")
+        show_points.triggered.connect(self.showPoints)
     
     def imageDisplay(self):
         ## Label (pixmap) for displaying images
         
         self.image_layout = QVBoxLayout()
+        self.image_layout.setAlignment(QtCore.Qt.AlignmentFlag.AlignTop)
+        #self.image_layout.setSpacing(0)
         
         self.image = QtWidgets.QLabel()
-        self.image.setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
+        self.image.setStyleSheet("border: 1px solid black;") 
+        self.image.setFixedSize(500,300) #QtCore.QSize(500,300))
         self.image_layout.addWidget(self.image)
         
+        
+        #button_frame = QFrame()
+        #button_frame.setFrameStyle(QFrame.StyledPanel)
+        #button_frame.setContentsMargins(1,1,1,1)
+        
         button_layout = QHBoxLayout()
+        #button_frame.setLayout(button_layout)
+        
+        
+        button_layout.addStretch()
+        #button_layout.setSpacing(0)
+        
         backButton = QToolButton()
         backButton.setArrowType(QtCore.Qt.LeftArrow)
-        backButton.setFixedSize(100,30)
+        backButton.setFixedSize(30,30)
         backButton.clicked.connect(self.browseBackward)
         button_layout.addWidget(backButton)
         
         forwardButton = QToolButton()
         forwardButton.setArrowType(QtCore.Qt.RightArrow)
-        forwardButton.setFixedSize(100,30)
+        forwardButton.setFixedSize(30,30)
         forwardButton.clicked.connect(self.browseForward)
         button_layout.addWidget(forwardButton)
         
-        self.image_layout.addLayout(button_layout)        
-        self.generalLayout.addLayout(self.image_layout,0,2)     
-    
+        trash_button = QPushButton()
+        trash_button.setFixedSize(30,30)
+        pixmapi = QStyle.StandardPixmap.SP_TrashIcon
+        icon = self.style().standardIcon(pixmapi)
+        trash_button.setIcon(icon)
+        button_layout.addWidget(trash_button)
+        
+        edit_button = QPushButton()
+        edit_button.setFixedSize(30,30)
+        pixmapi = QStyle.StandardPixmap.SP_DialogResetButton
+        icon = self.style().standardIcon(pixmapi)
+        edit_button.setIcon(icon)
+        button_layout.addWidget(edit_button)
+        
+        button_layout.addStretch()
+        #self.image_layout.addWidget(button_frame)
+        self.image_layout.addLayout(button_layout) 
+        
+        return self.image_layout          
     
     def displayImage(self,file_path):
         pixmap = QtGui.QPixmap(file_path)
-        pixmap = pixmap.scaledToWidth(self.width())
+        pixmap = pixmap.scaledToWidth(self.image.width())
         self.image.setPixmap(pixmap)
+    
+    
+    def imageInfo(self):
+        self.info = QLabel()
+        self.info.setFixedSize(500,200)
+        self.info.setStyleSheet("border: 1px solid black;") 
+        self.info_layout = QHBoxLayout()
+        self.info.setAlignment(QtCore.Qt.AlignCenter)
+        self.info_dict = {"Mouse:":"Anonymouse",
+                          "Gender:":"Male",
+                          "Genotype:":"Opto",
+                            "Image:":"Img_example.jpg",
+                            "Profile Score:":0.89,
+                            "Key Point Score":0.97}
         
+        temp_text = ""
+        for key,value in self.info_dict.items():
+            temp_text += "{} \t \t \t {} \n".format(key,value)
+            
+        self.info.setText(temp_text)
+        self.info_layout.addWidget(self.info)
+        return self.info_layout
+    
     def selectImage(self):
         ## Browse and select image to display
         tkinter.Tk().withdraw()
