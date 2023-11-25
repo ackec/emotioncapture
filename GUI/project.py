8cc7f5fca7ff8eef8be0dc917251b57555b752da
Victor Nossborn <victor.nossborn@hotmail.com>
2023-11-25 14:52:48 +0100

    Switched to using a stacked widget for project dialog

--- GUI/project.py
+++ GUI/project.py
@@ -1,7 +1,7 @@
 from enum import Enum
 
 from PyQt5.QtWidgets import (QLabel, QSizePolicy, QFrame, QDialog, QWidget,
-                             QVBoxLayout, QPushButton)
+                             QVBoxLayout, QPushButton, QStackedWidget)
 
 from config import DIALOG_WIDTH, DIALOG_HEIGHT
 
@@ -9,6 +9,7 @@ __all__ = ["ProjectDialog", "ProjectMode"]
 
 
 class ProjectMode(Enum):
+    """ Possible states of the project management. """
     NEW = 0
     """ New project screen. """
     PROCESS = 1
@@ -34,15 +35,13 @@ class DialogPlaceHolder(QWidget):
         self.main_layout.addWidget(text)
 
         btn = QPushButton("Switch")
-        btn.clicked.connect(lambda: self.parent().switch(next))
+        btn.clicked.connect(lambda: self.parent().parent().switch(next))
         self.main_layout.addWidget(btn)
 
         self.setLayout(self.main_layout)
 
 
 class ProjectDialog(QDialog):
-
-    # TODO: Use a QStackedWidget instead
 
     titles = ["Create new project",
               "Processing data",
@@ -57,24 +56,18 @@ class ProjectDialog(QDialog):
         self.setModal(True)  # Prevent loss of focus
 
         self.main_layout = QVBoxLayout()
-        self.displayed_widget = QWidget()
-        self.main_layout.addWidget(self.displayed_widget)
+
+        self.modes = QStackedWidget()
+        self.modes.addWidget(NewProject())
+        self.modes.addWidget(Processing())
+        self.modes.addWidget(ProcessingFailed())
+
+        self.main_layout.addWidget(self.modes)
         self.setLayout(self.main_layout)
 
-    def switch(self, mode: ProjectMode, **kwargs):
-        if mode == ProjectMode.NEW:
-            new_widget = NewProject(**kwargs)
-        elif mode == ProjectMode.PROCESS:
-            new_widget = Processing(**kwargs)
-        elif mode == ProjectMode.ERROR:
-            new_widget = ProcessingFailed(**kwargs)
-        else:
-            raise ValueError("Incorrect mode supplied.")
-
+    def switch(self, mode: ProjectMode):
         self.setWindowTitle(self.titles[mode.value])
-        self.main_layout.replaceWidget(self.displayed_widget, new_widget)
-        self.displayed_widget.setParent(None)
-        self.displayed_widget = new_widget
+        self.modes.setCurrentIndex(mode.value)
 
     def show(self, mode: ProjectMode):
         self.switch(mode)
