0d1313081f34f917528de6093abfab2d5725fa7e
johmag2 <johan.o.magnusson@me.com>
2023-11-29 12:01:59 +0100

    ListWidget early impl.

--- GUI/main.py
+++ GUI/main.py
@@ -1,5 +1,4 @@
-import sys
-
+import sys, os
 from PyQt5.QtWidgets import (QMainWindow, QWidget, QStyle, QApplication,
                              QHBoxLayout, QVBoxLayout)
 
@@ -17,7 +16,7 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("Mouse")
         self.setGeometry(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT)
         self.setMinimumSize(WINDOW_WIDTH, WINDOW_HEIGHT)
-
+                                                
         self.central_widget = QWidget(self)
         self.setCentralWidget(self.central_widget)
 
@@ -29,7 +28,14 @@ class MainWindow(QMainWindow):
 
         file_menu = menu_bar.addMenu("&File")
         edit_menu = menu_bar.addMenu("&Edit")
-
+        
+        ##  Temp menu to test functions (To be removed)
+        tempMenu = menu_bar.addMenu("&Temp")
+        
+        load_folder = tempMenu.addAction("Select Folder")
+        load_folder.triggered.connect(self.file_list.select_folder)
+        ##
+        
         # Create new project button
         new = file_menu.addAction("New Project")
         pixmapi = QStyle.StandardPixmap.SP_FileDialogNewFolder
@@ -50,25 +56,26 @@ class MainWindow(QMainWindow):
         main_layout = QHBoxLayout()
 
         # Components
-        file_list = ImageFileList()
-        image_viewer = ImageViewer()
-        image_metadata_viewer = ImageMetadataViewer()
+        self.image_viewer = ImageViewer()
+        self.file_list = ImageFileList(self.image_viewer)
+        self.image_metadata_viewer = ImageMetadataViewer()
 
         # Dialogs
         self.project_dialog = ProjectDialog()
         self.editor_dialog = ImageEditorDialog()
 
         # Left side
-        main_layout.addWidget(file_list, 40)
+        main_layout.addWidget(self.file_list, 40)
 
         # Right side
         right_side_layout = QVBoxLayout()
-        right_side_layout.addWidget(image_viewer, 65)
-        right_side_layout.addWidget(image_metadata_viewer, 35)
+        right_side_layout.addWidget(self.image_viewer, 65)
+        right_side_layout.addWidget(self.image_metadata_viewer, 35)
         main_layout.addLayout(right_side_layout, 60)
 
         self.central_widget.setLayout(main_layout)
 
+            
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
--- GUI/validation.py
+++ GUI/validation.py
@@ -1,5 +1,11 @@
 from PyQt5.QtWidgets import QLabel, QSizePolicy, QFrame, QWidget
+from PyQt5 import QtWidgets
+from PyQt5 import QtCore,QtGui
+import tkinter
+from tkinter import filedialog
+import os
 
+from data import *
 
 # List of packages that are allowed to be imported
 __all__ = ["ImageMetadataViewer", "ImageControl",
@@ -18,15 +24,84 @@ class PlaceHolder(QLabel):
         self.setFrameStyle(QFrame.Shape.Panel | QFrame.Shadow.Sunken)
 
 
-class ImageFileList(PlaceHolder):
+class ImageFileList(QtWidgets.QListWidget):
+    def __init__(self,image_viewer):
+        super().__init__()  #"Image File list"
+        
+        self.image_viewer = image_viewer
+        self.current_index = 0
+        self.pictures = []
+        self.image_extensions = [".jpg",".png"] ## Allowed file extensions for loading images
+                                                ## Find better way to check if image or not (?)
+                                                
+        self.currentItemChanged.connect(self.select_list_item)
+        self.setViewMode(QtWidgets.QListView.ViewMode.IconMode)
+        self.setIconSize(QtCore.QSize(100,100))
+        self.setFixedWidth(450)
+        self.setSpacing(0)
+        self.setUniformItemSizes(True)
+        
+    
+    def select_folder(self):
+        ## Browse and select folder to display images from
+        tkinter.Tk().withdraw()
+        folder_path = filedialog.askdirectory()
+        
+        if folder_path == None or folder_path == "":
+            return
+        
+        files = os.listdir(folder_path)
+        
+        mouse = MouseData()
+        mouse.gender = "None"
+        mouse.name = "Anonymouse"
+        for image_name in files:
+            ##Remove non picture files
+            if os.path.splitext(image_name)[1].lower() in self.image_extensions:
+                image_data = MouseImageData()
+                image_data.mouse = mouse
+                image_data.filename = image_name
+                image_data.path = folder_path + "/" + image_name
+                
+                self.pictures.append(image_data)
+        
+        self.current_index = 0
+        self.image_viewer.display_image(self.pictures[self.current_index])
+            
+        for image_data in self.pictures:
+            self.add_to_list(image_data)
+            
+    def add_to_list(self,image_data:MouseImageData):
+        name = image_data.filename
+        icon = QtGui.QIcon(image_data.path)
+        new_item = QtWidgets.QListWidgetItem(icon,name)
+        
+        self.addItem(new_item)
+    
+    def select_list_item(self,item:QtWidgets.QListWidgetItem):
+        name = item.text()
+        ## Find image with correct name
+        image_data = [image_data for image_data in self.pictures if image_data.filename == name][0]
+        
+        self.image_viewer.display_image(image_data)
+        self.current_index = self.pictures.index(image_data)
+        
+        
+class ImageViewer(QLabel):
     def __init__(self):
-        super().__init__("File list")
-
-
-class ImageViewer(PlaceHolder):
-    def __init__(self):
-        super().__init__("Image viewer")
-
+        super().__init__() #"Image viewer"
+        #self.setText("Image viewer")
+        
+        self.setSizePolicy(QSizePolicy.Policy.Expanding,
+                           QSizePolicy.Policy.Expanding)
+        
+        self.setFrameStyle(QFrame.Shape.Panel | QFrame.Shadow.Sunken)
+        
+    def display_image(self,image_data:MouseImageData):
+        
+        pixmap = QtGui.QPixmap(image_data.path)
+        pixmap = pixmap.scaledToWidth(self.width())
+        self.setPixmap(pixmap)
 
 class ImageControl(PlaceHolder):
     def __init__(self):
